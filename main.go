25f875b170a4b7c011e316ad239a8b9aa179fd4e
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-09-15 17:52:07 -0300

    feat: creating a hello world with Echo framework

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
 )
 
+func home(c echo.Context) error {
+	return c.String(http.StatusOK, "home")
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	e := echo.New()
+
+	e.GET("/", home)
+
+	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 }
