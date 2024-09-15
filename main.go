70ba6d0fc0a802a3d249a0ebe6a55552f7810939
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-09-15 19:35:28 -0300

    feat: creating a minimal db conn and table creation

--- main.go
+++ main.go
@@ -5,13 +5,37 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type Computers struct {
+	ID    uint `gorm:"primaryKey"`
+	Model string
+	Brand string
+}
+
+func getDB() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
 func home(c echo.Context) error {
-	return c.String(http.StatusOK, "home")
+	return c.JSON(http.StatusOK, echo.Map{"message": "hello"})
 }
 
 func main() {
+	db, err := getDB()
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	db.AutoMigrate(&Computers{})
+
 	e := echo.New()
 
 	e.GET("/", home)
