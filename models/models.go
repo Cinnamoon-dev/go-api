61cae991627901455ddd820c665dc242e5ac7724
Karen-Ribeiro <mrkarenpro@gmail.com>
2024-09-21 21:48:32 -0300

    feat: adjusting db tables

--- models/models.go
+++ models/models.go
@@ -1,20 +1,22 @@
 package models
 
 type Trabalhador struct {
-	ID           uint64         `gorm:"primaryKey"`
-	Nome         string         `gorm:"not null;"`
-	Cpf          string         `gorm:"not null;"`
-	Empresa      []Empresa      `gorm:"ForeignKey:ID;"`
-	Departamento []Departamento `gorm:"ForeignKey:ID;"`
+	ID             uint64       `gorm:"primaryKey"`
+	Nome           string       `gorm:"not null;"`
+	Cpf            string       `gorm:"not null; unique"`
+	EmpresaID      uint64       `gorm:"not null"`                                                // Chave estrangeira que faz referência ao ID da tabela Empresa
+	Empresa        Empresa      `gorm:"foreignKey:EmpresaID;constraint:OnDelete:SET NULL;"`      // Define a relação com Empresa usando EmpresaID
+	DepartamentoID uint64       `gorm:"not null"`                                                // Chave estrangeira que faz referência ao ID da tabela Departamento
+	Departamento   Departamento `gorm:"foreignKey:DepartamentoID;constraint:OnDelete:SET NULL;"` // Define a relação com Departamento usando DepartamentoID
 }
 
 type Empresa struct {
 	ID          uint64 `gorm:"primaryKey"`
-	RazaoSocial string `gorm:"not null;"`
+	RazaoSocial string `gorm:"not null; unique"`
 	Cnpj        string `gorm:"not null;"`
 }
 
 type Departamento struct {
 	ID   uint64 `gorm:"primaryKey"`
-	Nome string `gorm:"not null;"`
+	Nome string `gorm:"not null; unique"`
 }
